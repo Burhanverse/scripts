1671af655a63a4a67b149d14f2a2b8d84a7ef1f3
Aqua <burhanverse@gmail.com>
2024-03-20 21:15:13 +0530

    Update blissify.sh

--- blissify.sh
+++ blissify.sh
@@ -2,7 +2,7 @@
 
 # repo init & sync source 
 rm -rf .repo/local_manifests
-repo init -u repo init -u https://github.com/BlissRoms/platform_manifest.git -b typhoon --git-lfs
+repo init -u https://github.com/BlissRoms/platform_manifest.git -b typhoon --git-lfs
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10
 
 # device tree
