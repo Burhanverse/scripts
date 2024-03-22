b1627c6971c828cb5d2f2a5cf35539d0375e620c
Aqua <burhanverse@gmail.com>
2024-03-22 17:06:47 +0530

    Update los.sh

--- los.sh
+++ los.sh
@@ -8,6 +8,14 @@
 # device tree
 git clone --depth 1 https://github.com/Burhanverse/life -b los device/xiaomi/lancelot
 
+# build user flags
+export KBUILD_BUILD_USER=Burhanverse
+export KBUILD_BUILD_HOST=burhancodes
+export BUILD_USERNAME=Burhanverse
+export BUILD_HOSTNAME=burhancodes
+export TARGET_KERNEL_BUILD_USER=Burhanverse
+export TARGET_KERNEL_BUILD_HOST=burhancodes
+
 # init build
 . build/envsetup.sh 
 
