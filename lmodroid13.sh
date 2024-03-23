42e950b1b63b88ea6c9383efd857753ba7e8f3d3
Aqua <burhanverse@gmail.com>
2024-03-24 04:44:43 +0530

    use 12 cores to sync

--- lmodroid13.sh
+++ lmodroid13.sh
@@ -7,7 +7,7 @@ cd lmo || exit # Exit if the directory doesn't exist
 # Initialize the repo
 repo init --depth=1 -u https://github.com/burhancodes/lmodroid.git -b thirteen --git-lfs
 # Sync the repo
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j12
 
 # Clone the device tree
 git clone --depth 1 https://github.com/burhancodes/device_lmodroid_lancelot -b 13 device/xiaomi/lancelot
