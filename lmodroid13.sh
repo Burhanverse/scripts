eb712154c81c049eaf1bea49801dd4bc0e25b72f
Aqua <burhanverse@gmail.com>
2024-03-24 02:36:20 +0530

    implement zyc-clang

--- lmodroid13.sh
+++ lmodroid13.sh
@@ -11,6 +11,24 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # Clone the device tree
 git clone --depth 1 https://github.com/burhancodes/device_lmodroid_lancelot -b 13 device/xiaomi/lancelot
+
+# Use latest ZyC-Clang
+API_URL="https://api.github.com/repos/ZyCromerZ/Clang/releases/latest"
+TARGET_DIR="prebuilts/clang/host/linux-x86/zyc-clang"
+ASSET_URL=$(curl -s $API_URL | jq -r '.assets[0].browser_download_url')
+# Check if the URL is empty or not
+if [ -z "$ASSET_URL" ]; then
+    echo "Failed to find asset download URL."
+    exit 1
+fi
+# Download latest release
+curl -L -o latest_release.tar.gz "$ASSET_URL"
+mkdir -p "$TARGET_DIR"
+# Extract
+tar -xzf latest_release.tar.gz -C "$TARGET_DIR"
+# Clean up
+rm latest_release.tar.gz
+
 # Source the build environment
 . build/envsetup.sh
 
