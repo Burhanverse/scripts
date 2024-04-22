fbef4b3fc4471af7eac06e55c6b2c229a707ad01
Aqua <burhanverse@gmail.com>
2024-04-22 20:25:30 +0530

    Update lmodroid13.sh

--- lmodroid13.sh
+++ lmodroid13.sh
@@ -1,5 +1,5 @@
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
-repo init --depth=1 --no-repo-verify -u https://github.com/burhancodes/lmodroid.git -b thirteen --git-lfs && \
+repo init --depth=1 --no-repo-verify -u https://github.com/burhancodes/lmodroid.git -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault && \
 git clone https://github.com/burhancodes/local_manifest --depth 1 -b main .repo/local_manifests && \
 /opt/crave/resync.sh && \
 source build/envsetup.sh && \
